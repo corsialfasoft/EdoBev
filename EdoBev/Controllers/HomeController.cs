3e09738e197184693e92c70d048430cd58ab5081
JokerJackassJam <edo.bevione@gmail.com>
2018-04-05 12:33:28 +0200

    init project con mod di prova

---
+++ EdoBev/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EdoBev.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            @ViewBag.MioContenuto = "Sono in index!";
+            return View();
+        }
+
+        public ActionResult About() {
+            ViewBag.Message = "Your application description page.";
+            @ViewBag.MioContenuto = "Esempio";
+            return View();
+        }
+
+        public ActionResult Contact() {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ EdoBev/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EdoBev.MvcApplication" Language="C#" %>
